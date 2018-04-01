2f1bb1ea7e2671455a9b31450beeabfbe888ca6e
phatpham9 <phatpham9@gmail.com>
2018-04-01 18:51:57 +0700

    fix(close #2): can't get data for the current date

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -12,85 +12,101 @@ class Calculator extends Component {
 
     this.state = {
       top: 10,
-      from: '2017-01-01',
-      to: '2017-12-31',
       min: undefined,
       max: undefined,
+      from: '2017-01-01',
+      to: '2017-12-31',
       ignores: '',
-      inProgress: false,
       response: undefined,
+      inProgress: false,
     }
-    this.onChange = this.onChange.bind(this);
+
+    this.change = this.change.bind(this);
     this.submit = this.submit.bind(this);
   }
 
-  onChange(e) {
-		const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+  change(e) {
+    const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
     const name = e.target.name;
-		this.setState({ [name]: value });
+
+		this.setState({
+      [name]: value,
+    });
   }
+
   submit(e){
     e.preventDefault();
-    const top = this.state.top,
-          min = this.state.min,
-          max = this.state.max,
-          from = this.state.from,
-          to = this.state.to,
-          ignores = this.state.ignores;
-    this.setState({
-      inProgress: true,
-      response: undefined,
-    }, async () => {
 
-      const response = await CryptoROI.calculate({top, from, to, max, min, ignores: ignores ? ignores.split(',') : [],});
+    this.setState({
+      response: undefined,
+      inProgress: true,
+    }, async () => {
+      const { top, from, to, min, max, ignores } = this.state;
+
+      const response = await CryptoROI.calculate({
+        top,
+        from,
+        to,
+        min,
+        max,
+        ignores: ignores ? ignores.split(',') : [],
+      });
+
       this.setState({
         response,
         inProgress: false,
       });
     });
   }
+
   render() {
     return (
       <div className="calculator-wrapper">
         <Form onSubmit={this.submit}>
           <FormGroup>
-            <Label for="top">Top</Label>
-            <Input type="text" name="top" id="topSelect" value={this.state.top} onChange={this.onChange} required></Input>
+            <Label for="top">Top coins</Label>
+            <Input type="text" name="top" id="topSelect" value={this.state.top} onChange={this.change} required></Input>
           </FormGroup>
-          <Row className="form-group">
-            <Col xs="6">
-              <Label for="min">Min</Label>
-              <Input type="text" name="min" id="min" value={this.state.min} placeholder="Min" onChange={this.onChange}></Input>
-            </Col>
-            <Col xs="6">
-              <Label for="max">Max</Label>
-              <Input type="text" name="max" id="max" value={this.state.max} placeholder="Max" onChange={this.onChange}></Input>
-            </Col>
-          </Row>
+
           <Row className="form-group">
             <Col xs="6">
               <Label for="from">From</Label>
-              <Input type="date" value={this.state.from} name="from" id="from" placeholder="From date" onChange={this.onChange} required/>
+              <Input type="date" value={this.state.from} name="from" id="from" placeholder="From date" onChange={this.change} required/>
             </Col>
             <Col xs="6">
               <Label for="dateE">To</Label>
-              <Input type="date" value={this.state.to} name="to" id="to" placeholder="To date" onChange={this.onChange} required/>
+              <Input type="date" value={this.state.to} name="to" id="to" placeholder="To date" onChange={this.change} required/>
             </Col>
           </Row>
+
+          <Row className="form-group">
+            <Col xs="6">
+              <Label for="min">Min price (optional)</Label>
+              <Input type="text" name="min" id="min" value={this.state.min} placeholder="Min" onChange={this.change}></Input>
+            </Col>
+            <Col xs="6">
+              <Label for="max">Max price (optional)</Label>
+              <Input type="text" name="max" id="max" value={this.state.max} placeholder="Max" onChange={this.change}></Input>
+            </Col>
+          </Row>
+
           <FormGroup>
-              <Label for="ignores">Except coins</Label>
-              <Input type="search" value={this.state.ignores} name="ignores" id="ignores" placeholder="Enter a coin you want to ignore." onChange={this.onChange}/>
-              <FormText color="muted">
-                Each coin separated by comma. Like this BTC,ADA,RXP
-              </FormText>
-            </FormGroup>
+            <Label for="ignores">Except (optional)</Label>
+            <Input type="search" value={this.state.ignores} name="ignores" id="ignores" placeholder="Enter a coin you want to ignore." onChange={this.change}/>
+            <FormText color="muted">
+              Coins are separated by comma. For example: 'USDT,DOGE'
+            </FormText>
+          </FormGroup>
+
           <FormGroup>
-            <Button color="primary" type="submit" block busy={this.state.inProgress} busyText="Requesting...">Calculate</Button>
+            <Button color="primary" type="submit" block busy={this.state.inProgress} busyText="Calculating...">Calculate</Button>
           </FormGroup>
         </Form>
-        { this.state.response &&
+
+        { this.state.response && (
           <div className="response-table">
             <span>If you put {`$${this.state.response.investmentOfEach}`} in each coin, you will make...</span>
+
             <Table bordered>
               <thead>
                 <tr>
@@ -98,14 +114,15 @@ class Calculator extends Component {
                   <th colSpan="2">{`From ${this.state.from}`}</th>
                   <th>Multiplier</th>
                   <th colSpan="2">{`To ${this.state.to}`}</th>
-                </tr> 
-              </thead> 
+                </tr>
+              </thead>
+
               <tbody>
                 { this.state.response.coins &&
-                    this.state.response.coins.map((coin, index) => 
+                    this.state.response.coins.map((coin, index) =>
                       <tr key={index}>
                         <td>
-                          <img src={`https://raw.githubusercontent.com/cjdowner/cryptocurrency-icons/master/32/color/${coin.symbol.toLowerCase()}.png`} alt="" /> 
+                          <img src={`https://raw.githubusercontent.com/cjdowner/cryptocurrency-icons/master/32/color/${coin.symbol.toLowerCase()}.png`} alt="" />
                           {coin.name || 'N/A'}
                         </td>
                         <td>{`#${coin.lastIndex}`}</td>
@@ -125,7 +142,7 @@ class Calculator extends Component {
               </tbody>
             </Table>
           </div>
-        }
+        )}
       </div>
     );
   }
